22395984298a7e1130cf74f244a38ab870320eaa
Felipe Alves <vieira.felipe.alves@gmail.com>
2025-05-21 21:50:38 -0300

    DatePipe

--- src/app/banner/banner.component.ts
+++ src/app/banner/banner.component.ts
@@ -1,11 +1,13 @@
+import { DatePipe } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-banner',
-  imports: [],
+  imports: [DatePipe],
   templateUrl: './banner.component.html',
   styleUrl: './banner.component.css'
 })
 export class BannerComponent {
 
+  dataAtual = new Date();
 }
